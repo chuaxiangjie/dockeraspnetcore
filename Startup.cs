e710d3faf4acbc6c40321ec1e676a8d7379509e0
Xiang Jie Chua <ran_dom1990@hotmail.com>
2021-02-08 11:36:10 +0800

    update

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace dockeraspnetcore
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dockeraspnetcore v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
